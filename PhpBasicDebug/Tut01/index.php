a8605b2fdbf04b232216b08dca0817f759b95bc9
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-08-23 06:01:29 +1200

    Update index.php

--- PhpBasicDebug/Tut01/index.php
+++ PhpBasicDebug/Tut01/index.php
@@ -18,5 +18,30 @@
 +--------+--------------+------------------------------------------------------------------------------------------+
 */
 
+error_reporting(E_ALL|E_STRICT|E_DEPRECATED);
+ini_set('display_errors', 1);
+
+// E_ERROR - Call to undefined function
+does_not_exists(); //Fatal error: Call to undefined function does_not_exists()
+
+// E_WARNING - failed to open stream: No such file or directory
+fopen('does.not.exists.txt', 'r');  //Warning: fopen(does.not.exists.txt): failed to open stream: No such file or directory
+
+// E_PARSE - syntax error, unexpected end of file
+function my_func() {  //Parse error: syntax error, unexpected end of file
+
+// E_NOTICE - Undefined variable: not_defined_var
+print $not_defined_var;  //Notice: Undefined variable: not_defined_var
+
+// E_STRICT
+class Myclass{	
+	public static $staticX=1;
+}
+$ObjMyclass = new Myclass();
+$ObjMyclass->$staticX;  //Notice: Undefined variable: staticX in C:\xampp\htdocs\prac\index.php on line 186
+				    //Fatal error: Cannot access empty property in C:\xampp\htdocs\prac\index.php on line 186
+
+//E_DEPRECATED - Function split() is deprecated
+var_dump(split(',', 'one,two,three')); // Deprecated: Function split() is deprecated
 
 ?>
