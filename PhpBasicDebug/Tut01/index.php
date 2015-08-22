c7721be6818f4c2bbd0b8eab85e187ad43ea7d4a
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-08-23 03:47:33 +1200

    Create index.php

---
+++ PhpBasicDebug/Tut01/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| PHP has 16 errors reporting levels
+|--------------------------------------------------------------------------
+| Each level has constant name integer value. Declaring common error reporting levels below
++--------+--------------+------------------------------------------------------------------------------------------+
+| Value  | Constant     | Description                                                                              |
++--------+--------------+------------------------------------------------------------------------------------------+
+| 1      | E_ERROR      | Fatal run-time errors (crash)                                                            |
+| 2      | E_WARNING    | Run-time warnings, dose not halt                                                         |
+| 4      | E_PARSE      | Compile-time errors, dose not execute                                                    |
+| 8      | E_NOTICE     | Run-time notices, dose not halt but there is something wrong                             |
+| 2048   | E_STRICT     | Compile-time notice, dose not halt but reinforces forward compatibilty and best practice |
+| 8192   | E_DEPRECATED | Run-time notice, dose not halt but will not work in future PHP versions                  |
+| 32767  | E_ALL        | All errors, warnings and notices, except E_STRICT                                        |
++--------+--------------+------------------------------------------------------------------------------------------+
+*/
+
+
+?>
