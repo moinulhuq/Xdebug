6e3e7d970fb9a23ac021003474195de2e14163ac
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-08-24 14:01:12 +1200

    Update index_one.php

--- PhpBasicDebug/Tut07/index_one.php
+++ PhpBasicDebug/Tut07/index_one.php
@@ -4,10 +4,7 @@
 |--------------------------------------------------------------------------
 | Custom Error Handling - Method 04 (Exception Handling)
 |--------------------------------------------------------------------------
-| 1. Exception change the normal flow of program execution
-| 2. In PHP Exceptions are thrown and caught
-| 3. To catch an Exception surrownd you code in "try" block
-| 4. If Exception is thrown and not caught then PHP fatal Error occured
+| 1. Try, throw and catch
 */
 
 function checkNum($number) { //create function with an exception
