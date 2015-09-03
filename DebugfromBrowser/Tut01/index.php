45ad1cc44ebe958b5b0f10105fa524e1e09504e2
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-09-03 12:54:05 +1200

    Create index.php

---
+++ DebugfromBrowser/Tut01/index.php
@@ -0,0 +1,8 @@
+https://getfirebug.com/downloads/
+
+https://addons.mozilla.org/en-us/firefox/addon/firephp/
+
+https://github.com/firephp/firephp-core
+
+Extract "FirePHPCore" from "firephp-core-master.zip" and "FirePHPCore" will be in "firephp-core-master.zip\firephp-core-master\lib\" archive dir.
+
